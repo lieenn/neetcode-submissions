5ba9487bf736a5be1b37add2093c00e620461454
Lien Nguyen <lieenn2410@gmail.com>
2026-07-23 00:03:45 -0400

    Add: clone-graph - submission-7

---
+++ Data Structures & Algorithms/clone-graph/submission-7.py
@@ -0,0 +1,31 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        found = {}
+
+        def cloneNode(node):
+            if node is None:
+                return node
+            if node not in found:
+                new_node = Node(node.val)
+                found[node] = new_node
+                for neighbor in node.neighbors:
+                    new_n = cloneNode(neighbor)
+                    found[node].neighbors.append(new_n)
+            return found[node]
+        return cloneNode(node)
+        
+
+
+
+
+            
+        
+        
